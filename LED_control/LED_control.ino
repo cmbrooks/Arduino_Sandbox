ad44496a7c7d9a8f8d72b1e67c37169b01d89ec2
SuperSudo36 <codymb36@gmail.com>
2014-06-06 10:20:47 -0500

    Modes added, mode index is not incrementing

--- LED_control/LED_control.ino
+++ LED_control/LED_control.ino
@@ -1,42 +1,126 @@
 int blinkSpeed = 3000;
+int fadeAmount = 5;
+int fadeBrightness = 0;
+char* modeList[] = {"solid","fade","colorCycle"};
 
 void setup(){
-
-  pinMode(2,INPUT); //Signal from NXT
-  pinMode(3,OUTPUT); //Red
-  pinMode(4,OUTPUT); //Green
-  pinMode(5,OUTPUT); //Blue
   
+  Serial.begin(9600);
+  pinMode(2,INPUT); //Signal from NXT
+  pinMode(3,INPUT); //Breathe
+  pinMode(9,OUTPUT); //Red
+  pinMode(10,OUTPUT); //Green
+  pinMode(11,OUTPUT); //Blue
+  
+  Serial.println("Hello World");
 }
 
 void loop(){
 
   int NXTsignal = digitalRead(2);
-
-  //In teleop mode, LEDs are constantly purple
-  if(NXTsignal == LOW){
-    digitalWrite(3,HIGH); //LEDs are purple
-    digitalWrite(4,LOW);
-    digitalWrite(5,HIGH);
+  int modeChange = digitalRead(3);
+  int modeIndex = 0;
+  String mode = modeList[modeIndex];
+  
+  if(modeChange == HIGH){
+    
+    /***
+    *When the Mode changing button is
+    *pressed, the mode index increments to
+    *change to the next mode
+    ***/
+    modeIndex = modeIndex++;
+    delay(250);
   }
   
-  else{
+  if(mode == "solid"){
     
-    //Blinks the LEDs at an increacing pace when End Game starts
-
-    digitalWrite(3,HIGH);
-    digitalWrite(4,LOW);
-    digitalWrite(5,HIGH);
+    //LEDs remain a constant color
+    purple();
+    Serial.print("Mode is ");
+    Serial.println(mode);
     
-    delay(blinkSpeed);
+  }else if(mode == "fade"){
     
-    digitalWrite(3,LOW);
-    digitalWrite(4,LOW);
-    digitalWrite(5,LOW);
+    //Flades LED in and out
+    fadePurple();
+    Serial.print("Mode is ");
+    Serial.println(mode);
     
-    delay(blinkSpeed);
+  }else if(mode == "colorCycle"){
     
-    blinkSpeed = blinkSpeed * .8; //Shortens then next blink by 3/4
+    //Changes LED color every 2 seconds
+    red();
+    delay(2000);
+    blue();
+    delay(2000);
+    green();
+    delay(2000);
+    
+    Serial.print("Mode is ");
+    Serial.println(mode);
     
   }
+  
+  if(NXTsignal == HIGH){
+    
+    //Blinks the LEDs at an increacing pace
+    purple(); 
+    delay(blinkSpeed);
+    off();
+    delay(blinkSpeed);
+    blinkSpeed = blinkSpeed * .8; //Shortens then next blink
+  
+  }
+}  
+  
+
+
+//LED Preset
+void off(){
+  digitalWrite(9,LOW); //LEDs are purple
+  digitalWrite(10,LOW);
+  digitalWrite(11,LOW);
+}
+
+void purple(){
+  digitalWrite(9,HIGH); //LEDs are purple
+  digitalWrite(10,LOW);
+  digitalWrite(11,HIGH);
+}
+
+void red(){
+  digitalWrite(9,HIGH); //LEDs are Red
+  digitalWrite(10,LOW);
+  digitalWrite(11,LOW);
+}
+
+void blue(){
+  digitalWrite(9,LOW); //LEDs are Blue
+  digitalWrite(10,LOW);
+  digitalWrite(11,HIGH);
+}
+
+void green(){
+
+  digitalWrite(9,LOW); //LEDs are Green
+  digitalWrite(10,HIGH);
+  digitalWrite(11,LOW);
+
+}
+
+void fadePurple(){
+
+  analogWrite(9, fadeBrightness);    
+  analogWrite(11, fadeBrightness);  
+  // change the brightness for next time
+  fadeBrightness = fadeBrightness + fadeAmount;
+
+  // reverse the direction of the fading 
+  if (fadeBrightness == 0 || fadeBrightness == 255) {
+    fadeAmount = -fadeAmount ; 
+  }     
+ 
+  delay(60); 
+
 }
