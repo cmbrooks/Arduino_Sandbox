eeff08a1cdbe2adcb56195f383a3b948fa6ea5a0
cobrooks <codymb36@gmail.com>
2014-08-25 21:12:29 -0500

    Updated QSM template; Started LED_QSM for DM

--- Templates/QSM/QSM.ino
+++ Templates/QSM/QSM.ino
@@ -1,10 +1,48 @@
-String que[] = {"init"};
+/*Setup Variables here*/
+int currQue = 0;
+int writeQue = 0;
 
+/*Fill enum with all case names*/
+enum cases{
+  init,
+  idle,
+  case1,
+  case2,
+  leave
+};
+
+/*Begin Program*/
 void setup(){
   Serial.begin(9600);
-  //que.append("init");
+  String que[writeQue] = init;
+  writeQue++;
 }
 
 void loop(){
-  que.append("idle");
+  
+  switch(que[currQue]){
+    
+    case init:
+      /*Prepare program here*/
+      nextState = idle;
+      break;
+    
+    case idle:
+      if (input1){
+        que[writeQue] = case1;
+      }else if (input2){
+        que[writeQue] = case2;
+      }else{
+        que[writeQue] = idle;
+      }   
+    
+    case case1:
+      //Code for case1 here
+      break;
+    
+    case case2:
+      //Code for case2 here
+      break;
+      
+  }
 }
